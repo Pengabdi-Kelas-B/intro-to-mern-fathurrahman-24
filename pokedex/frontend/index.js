19345160c91d8626b7cb6c6a997b0f6f2219a01b
fathurrahman-24 <faturrahman2004678@gmail.com>
2024-10-19 17:04:57 +0700

    Add Tailwind CSS styling to PokemonCard and PokemonList components

--- pokedex/frontend/index.js
+++ pokedex/frontend/index.js
@@ -2,76 +2,78 @@ let pokemonData = [];
 
 // Fetch data from mock server
 async function fetchPokemon() {
-  try {
-    const response = await fetch("http://localhost:3000/pokemon");
-    if (!response.ok) {
-      throw new Error("http call failed");
+    try {
+        const response = await fetch("http://localhost:3000/pokemon");
+        if (!response.ok) {
+            throw new Error("http call failed");
+        }
+        const data = await response.json();
+        pokemonData = data;
+        renderApp();
+    } catch (error) {
+        console.error("Failed to fetch Pokemon data:", error);
+        renderApp();
     }
-    const data = await response.json();
-    pokemonData = data;
-    renderApp();
-  } catch (error) {
-    console.error("Failed to fetch Pokemon data:", error);
-    renderApp();
-  }
 }
 
 // Card component
 function PokemonCard(props) {
-  return React.createElement(
-    "div",
-    { className: "" },
-    React.createElement("img", { src: props.image, alt: props.name }),
-    React.createElement("h2", null, props.name),
-    React.createElement("p", null, `Type: ${props.types}`)
-  );
+    return React.createElement(
+        "div",
+        {
+            className: "bg-white rounded-lg shadow-2xl p-6 m-4 w-72 transition-transform transform hover:scale-105", // Kartu Pokemon
+        },
+        React.createElement("img", { src: props.image, alt: props.name, className: "mx-auto h-36 w-36" }), // Gambar Pokemon
+        React.createElement("h2", { className: "text-2xl font-bold text-center text-blue-700 mt-3" }, props.name), // Nama Pokemon
+        React.createElement("p", { className: "text-gray-500 text-center text-lg" }, `Type: ${props.types}`) // Tipe Pokemon
+    );
 }
 
 // List component
 function PokemonList() {
-  if (pokemonData.length === 0) {
-    return React.createElement(
-      "p",
-      { className: "text-center" },
-      "Loading Pokemon data..."
-    );
-  }
+    if (pokemonData.length === 0) {
+        return React.createElement(
+            "p",
+            { className: "text-center text-lg font-semibold" },
+            "Loading Pokemon data..."
+        );
+    }
 
-  return React.createElement(
-    "div",
-    { className: "flex flex-wrap justify-center" },
-    pokemonData.map((pokemon) =>
-      React.createElement(PokemonCard, {
-        key: pokemon.id,
-        name: pokemon.name,
-        types: pokemon.types.join("/"),
-        image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`,
-      })
-    )
-  );
+    return React.createElement(
+        "div",
+        { className: "flex flex-wrap justify-center" },
+        pokemonData.map((pokemon) =>
+            React.createElement(PokemonCard, {
+                key: pokemon.id,
+                name: pokemon.name,
+                types: pokemon.types.join("/"),
+                image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`,
+            })
+        )
+    );
 }
 
 // App component wrap header and list
 function App() {
-  return React.createElement(
-    "div",
-    { className: "" },
-    React.createElement(
-      "header",
-      { className: "" },
-      React.createElement(
-        "h1",
-        { className: "text-3xl text-center font-bold underline" },
-        "Pokedex"
-      )
-    ),
-    React.createElement(PokemonList, null)
-  );
+    return React.createElement(
+        "div",
+        { className: "max-w-screen-lg mx-auto p-4" }, // Center the content
+        React.createElement(
+            "header",
+            { className: "bg-gradient-to-r from-blue-600 to-purple-600 text-white p-6 rounded-lg shadow-lg" },
+            React.createElement(
+                "h1",
+                { className: "text-4xl text-center font-extrabold" },
+                "Pokedex"
+            )
+        ),
+        React.createElement(PokemonList, null)
+    );
 }
 
 // Function to render the app
 function renderApp() {
-  ReactDOM.render(React.createElement(App), document.getElementById("root"));
+    ReactDOM.render(React.createElement(App), document.getElementById("root"));
 }
 
 // Initial render
